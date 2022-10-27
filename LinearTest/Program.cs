1f43c6e17a087b7a849a6ad959f440c8e6b2ca0f
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-10-27 17:30:00 +0700

    create method main

--- LinearTest/Program.cs
+++ LinearTest/Program.cs
@@ -108,6 +108,51 @@ namespace LinearTest
         }
         static void Main(string[] args)
         {
+            Program myList = new Program();
+            int pilihanmenu;
+            char ch;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Menu Option");
+                    Console.WriteLine("=================");
+                    Console.WriteLine("1. Linear search");
+                    Console.WriteLine("2. binary search");
+                    Console.WriteLine("3. exit");
+                    Console.WriteLine("Enter your choice (1,2,3) : ");
+                    pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                    switch (pilihanmenu)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine("................");
+                            Console.WriteLine("Binary Search");
+                            Console.WriteLine("................");
+                            myList.input();
+                            myList.LinearSearch();
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine("................");
+                            Console.WriteLine("Binary Search");
+                            Console.WriteLine("................");
+                            myList.input();
+                            myList.LinearSearch();
+                            break ;
+                        case 3:
+                            Console.WriteLine("Exit");
+                            break;
+                    }
+                    Console.WriteLine("\nPilih menu lagi? (y/n): ");
+                    ch = char.Parse(Console.ReadLine().ToLower());
+                    Console.Clear();
+                } while (ch == 'y');
+
+                //to exit from the console
+                Console.WriteLine("\n\nPress return to exit. ");
+                Console.ReadLine();
+            } while (pilihanmenu != 0);
         }
     }
 }
