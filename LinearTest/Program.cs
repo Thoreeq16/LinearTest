71a3c0b2da6e49f8772022542a6fadc8efd00640
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-10-27 16:33:27 +0700

    melengkapi method input

--- LinearTest/Program.cs
+++ LinearTest/Program.cs
@@ -9,7 +9,7 @@ namespace LinearTest
     class Program
     {
         //array to be searched
-        int[] arr = new int[20]
+        int[] arr = new int[20];
         //number of element i n the array
         int n;
         //get the number of element to store in the array
@@ -17,14 +17,27 @@ namespace LinearTest
 
         public void input()
         {
-            while (true)
+            while(true)
+            {
                 Console.Write("enter the number of element in the array");
-            string s = Console.ReadLine();
-            n = Int32.Parse(s);
-            if ((n > 0) && (n <= 20))
-                break;
-            else
-                Console.WriteLine("\nArray should have minimum 1 and minimum 20 element.\n");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and minimum 20 element.\n");
+            }
+            //accept array element
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" Enter Array Element ");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine('<' + (i + 1) + ">";
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
         static void Main(string[] args)
         {
