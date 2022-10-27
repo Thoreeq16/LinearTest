88ee284ece3cac0c0e27a30dd2952f7c88018807
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-10-27 16:19:50 +0700

    add method input

--- LinearTest/Program.cs
+++ LinearTest/Program.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace LinearTest
 {
-    internal class Program
+    class Program
     {
+        //array to be searched
+        int[] arr = new int[20]
+        //number of element i n the array
+        int n;
+        //get the number of element to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+                Console.Write("enter the number of element in the array");
+            string s = Console.ReadLine();
+            n = Int32.Parse(s);
+            if ((n > 0) && (n <= 20))
+                break;
+            else
+                Console.WriteLine("\nArray should have minimum 1 and minimum 20 element.\n");
+        }
         static void Main(string[] args)
         {
         }
